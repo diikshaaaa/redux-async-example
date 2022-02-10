90b999797a13c6b12078e06e75554e8922612308
diikshaaaa <diksha.grover@crownstack.com>
2022-02-10 12:21:53 +0530

    learnt async in redux

---
+++ README.md
@@ -0,0 +1,3 @@
+npm i react-redux --save
+npm install redux-thunk --save 
+npm i redux-logger --save
---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { selectSubreddit, fetchPostsIfNeeded, invalidateSubreddit } from './redux/actions'
+import Picker from './components/Picker'
+import Posts from './components/Posts'
+
+class App extends Component {
+  static propTypes = {
+  // Static propTypes are called directly on the class (here App) - without creating an instance/object of the class.
+    selectedSubreddit: PropTypes.string.isRequired,
+    posts: PropTypes.array.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    lastUpdated: PropTypes.number,
+    dispatch: PropTypes.func.isRequired
+  }
+
+  componentDidMount() {
+  // Mounting means putting elements into the DOM.
+  // componentDidMount () is a hook that gets invoked right after a React component has been mounted aka after the first render () 
+    const { dispatch, selectedSubreddit } = this.props
+    dispatch(fetchPostsIfNeeded(selectedSubreddit))
+  }
+
+  componentDidUpdate(prevProps) {
+  // component is updated whenever there is a change in the component's state or props.
+  // componentDidUpdate method is called after the component is updated in the DOM.
+    if (prevProps.selectedSubreddit !== this.props.selectedSubreddit) {
+      const { dispatch, selectedSubreddit } = this.props
+      dispatch(fetchPostsIfNeeded(selectedSubreddit))
+    }
+  }
+
+  handleChange = nextSubreddit => {
+    this.props.dispatch(selectSubreddit(nextSubreddit))
+  }
+
+  handleRefreshClick = e => {
+    e.preventDefault()
+
+    const { dispatch, selectedSubreddit } = this.props
+    dispatch(invalidateSubreddit(selectedSubreddit))
+    dispatch(fetchPostsIfNeeded(selectedSubreddit))
+  }
+
+  render() {
+    const { selectedSubreddit, posts, isFetching, lastUpdated } = this.props
+    const isEmpty = posts.length === 0
+    return (
+      <div>
+        <Picker value={selectedSubreddit}
+                onChange={this.handleChange}
+                options={[ 'reactjs', 'frontend' ]} />
+        <p>
+          {lastUpdated &&
+            <span>
+              Last updated at {new Date(lastUpdated).toLocaleTimeString()}.
+              {' '}
+            </span>
+          }
+          {!isFetching &&
+            <button onClick={this.handleRefreshClick}>
+              Refresh
+            </button>
+          }
+        </p>
+        {isEmpty
+          ? (isFetching ? <h2>Loading...</h2> : <h2>Empty.</h2>)
+          : <div style={{ opacity: isFetching ? 0.5 : 1 }}>
+              <Posts posts={posts} />
+            </div>
+        }
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  const { selectedSubreddit, postsBySubreddit } = state
+  const {
+    isFetching,
+    lastUpdated,
+    items: posts
+  } = postsBySubreddit[selectedSubreddit] || {
+    isFetching: true,
+    items: []
+  }
+
+  return {
+    selectedSubreddit,
+    posts,
+    isFetching,
+    lastUpdated
+  }
+}
+
+export default connect(mapStateToProps)(App)
